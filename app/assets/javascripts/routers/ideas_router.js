0c7f70d4b15471e047e04830bd86e83c84ec3d87
jasperknowsbest <edward.jasper@flatironschool.com>
2014-10-06 02:32:57 -0400

    need to bind collenction

--- app/assets/javascripts/routers/ideas_router.js
+++ app/assets/javascripts/routers/ideas_router.js
@@ -4,8 +4,14 @@ IdeaVoter.Routers.Ideas = Backbone.Router.extend({
 		'ideas/:id' : 'show'  
 	},
 
+	initialize: function(){
+		this.collection = new IdeaVoter.Collections.Ideas
+		this.collection.fetch();
+	},
+	
+
 	index: function() {
-		var view = new IdeaVoter.Views.IdeasIndex()
+		var view = new IdeaVoter.Views.IdeasIndex({collection: this.collection})
 		$('#container').html(view.render().el);
 	},
 
--- app/assets/javascripts/views/ideas/ideas_index.js
+++ app/assets/javascripts/views/ideas/ideas_index.js
@@ -1,10 +1,16 @@
 IdeaVoter.Views.IdeasIndex = Backbone.View.extend({
+	initialize: function() {
+    this.collection.on('reset', this.render);
+	},
+
 	render: function() {
-    this.renderTemplate();
+    this.renderTemplate(this.collection);
     return this;
 	},
 
+
+
 	renderTemplate: function() {
-    this.$el.html(HandlebarsTemplates['ideas/index'])
+    this.$el.html(HandlebarsTemplates['ideas/index']({ collection: this.collection }))
 	}
 });
